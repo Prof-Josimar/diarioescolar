38f2da0ed01fbf131f97fc86d8f60031eec19404
profjosimar <josimar.ribeiro@eteparacambi.com.br>
2025-11-16 07:55:35 -0300

    Reindex by CMD

--- reup.cmd
+++ reup.cmd
@@ -1,3 +1,28 @@
-git rm -r --cached .
+@echo off
+echo ============================================
+echo ===  FORCAR GIT A REINDEXAR TUDO (.CMD)  ===
+echo ============================================
+echo.
+
+REM ---> 1. Ajustar datas (opcional)
+REM remove REM caso queira atualizar datas
+REM for /r %%f in (*) do (
+REM    echo tocando %%f
+REM    copy /b "%%f" +,, "%%f" >nul
+REM )
+
+REM ---> 2. Remover arquivos do index (sem apagar fisicamente)
+echo Limpando index do Git...
+git rm -r --cached . >nul
+
+REM ---> 3. Adiciona tudo novamente
+echo Adicionando arquivos...
 git add .
-git commit -m "Reindex all files"
+
+REM ---> 4. Commit
+echo Realizando commit...
+git commit -m "Reindex by CMD"
+
+echo.
+echo ==== PRONTO! Git vai tratar tudo como novo. ====
+pause
